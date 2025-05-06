4c81d50afea10b9a41601e9602947db688d75207
Andyli807 <110069737+Andyli807@users.noreply.github.com>
2025-05-06 12:20:12 +1200

    Feature/admin (#18)
    * Upload API
    
    About the users and bookings API related to the admin interface
    
    * UPLOAD API
    
    admin_payment and admin_report API
    
    * identity authentication(Not implemented)

---
+++ backend/routes/admin/user.admin.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../../controllers/admin/user.admin.controller.js";
+
+const router = express.Router();
+
+
+// GET /api/admin/users
+router.get("/", getAllUsers);
+// GET /api/admin/users/user_id
+router.get("/:id", getUserById);
+// PUT /api/admin/users/user_id
+// {
+//   "email": "XXXX"
+// }
+router.put("/:id", updateUser);
+// DELETE /api/admin/users/user_id
+router.delete("/:id", deleteUser);
+
+export default router;
+//Add identity authentication and administrator authority verification
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { createUser, getUsers, deleteUser, getProviders, searchProviders } from "../controllers/user.controller.js";
+
+// import { createUser, getUsers, deleteUser } from "../controllers/user.controller.js";
+import { getProviders, searchProviders } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.delete("/:id", deleteUser);
+// router.post("/", createUser);
+// router.get("/", getUsers);
+// router.delete("/:id", deleteUser);
+
 router.get("/providers", getProviders); // GET /api/providers
 router.post('/providers/search', searchProviders);
+
 
 export default router;
