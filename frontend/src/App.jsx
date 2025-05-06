2f87a11d2efc54b84136325afc7a84fe837ac334
Andyli807 <110069737+Andyli807@users.noreply.github.com>
2025-05-06 23:12:00 +1200

    Merge: Dealing with code conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Box, VStack, } from "@chakra-ui/react"
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/Homepage'
-import Bookingpage from './pages/Bookingpage'
+import BookingPage from './pages/Bookingpage'
 import Navbar from './components/Navbar'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import UserProfilePage from './pages/UserProfilePage'
 const App = () => {
   return (
 
@@ -13,10 +14,11 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/booking" element={<Bookingpage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-
+        <Route path="/booking" element={<BookingPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/inbox' element={<UserProfilePage />} />
+        <Route path='/profile' element={<UserProfilePage defaultTab="profile" />} />
       </Routes>
     </Box>
 
