af9171d3cefbb18909eba5f8e1361e90f8800885
Andyli807 <110069737+Andyli807@users.noreply.github.com>
2025-05-07 15:22:16 +1200

    AUTH MIDDLE WARE TOKEN add Authorization mode

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,15 +4,16 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// POST /register
+// POST /api/auth/registers
 router.post("/register", registerUser);
 
-// POST /login
+// POST /api/auth/login
 router.post("/login", loginUser);
 
-// GET /me - 获取当前登录用户信息
+// GET /api/auth/me - 获取当前登录用户信息
 router.get("/me", authMiddleware, getCurrentUser);
 
+// GET /api/auth/verify-email
 router.get("/verify-email", verifyEmail);
 
 export default router;
