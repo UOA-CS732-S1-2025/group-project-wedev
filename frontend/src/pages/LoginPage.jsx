9d4f49fa9b7dafaf9e75e5499f31df942e3d2c7b
Ashutosh Singh <singhashutoshun1998@gmail.com>
2025-05-13 23:24:58 +1200

    log bg upd

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -92,24 +92,31 @@ const LoginPage = () => {
     },
   });
 
-  const canSwipe = email !== '' && password !== ''; // added: only enable swipe when fields filled
+  const canSwipe = email !== '' && password !== '';
 
   return (
-    <Box
-      position="relative"
-      minH="90vh"
-      bgGradient="linear(to-br, green.100, green.50)"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      px={4}
-      overflow="hidden"
-    >
-      <Box position="absolute" top="-80px" right="-100px" zIndex={0} opacity={0.15}>
-        <svg width="500" height="500" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#48BB78" d="M50.4,-61.2C65.5,-49.7,77.7,-32.4,77.6,-16.1C77.6,0.2,65.3,15.7,54.4,31.6C43.4,47.5,33.8,63.8,20.2,66.2C6.5,68.5,-11.2,56.9,-27.2,46.1C-43.3,35.2,-57.8,25.1,-66.7,9.5C-75.5,-6.1,-78.6,-28.1,-68.3,-42.6C-58,-57.1,-34.2,-64,-12.4,-61.6C9.4,-59.2,18.8,-47.7,50.4,-61.2Z" transform="translate(100 100)" />
-        </svg>
-      </Box>
+       <Box
+         position="relative"
+         minH="90vh"
+         bgGradient="linear(to-br, green.100, green.50)"
+         display="flex"
+         alignItems="center"
+         justifyContent="center"
+         px={4}
+         overflow="hidden"
+       >
+         <Image
+           src="https://urbaneaseproject.s3.us-east-1.amazonaws.com/blob.png"
+           alt="Background Blob"
+           position="absolute"
+           top="0"
+           left="0"
+           w="100%"
+           h="100%"
+           objectFit="cover"
+           zIndex={0}
+           opacity={0.4}
+         />
 
       <Box
         zIndex={1}
